2864416bd531f0cdf5ffa604879256c8d4225c21
Sahil Saroop <ssaroop@kinaxis.com>
2019-11-06 16:16:38 -0500

    feat: Add test project

---
+++ Hello.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hello.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
